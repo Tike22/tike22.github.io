b68dcd5581293e71edbcc093391b2042f38e9685
Tike22 <38795270+Tike22@users.noreply.github.com>
2020-12-15 05:54:12 -0500

    Final Submit

--- 364 Proj/main.js
+++ 364 Proj/main.js
@@ -5,9 +5,13 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+for(let i = 1; i <= 5; i++) {
+    
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg'); /* Creating image tag for html and in the image tag it is creating the source attribute.Then it is creating a name for each image and replacing the index as the number for the image  */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
-
-/* Wiring up the Darken/Lighten button */
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(current) {
+      displayedImage.src = current.target.src; // selecting the image pathway currently being clicked and setting the display image pathway to that
+    }
+}
